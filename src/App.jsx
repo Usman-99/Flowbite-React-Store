9bec5cbd21c79255e1556fbf24e095cbb08df73a
Usman-99 <usmanyousuf1137@gmail.com>
2024-12-05 17:47:43 +0500

    creating dynamic route for product details page

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,28 @@
 import React from "react";
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
-import './App.css'
-import Navbar from './components/Navbar'
-import Home from "./components/Home"
-import Product from "./components/Product"
-import Cart from "./components/Cart"
-import { CartProvider } from "./context/cartContext"; 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Product from "./components/Product";
+import Cart from "./components/Cart";
+import { CartProvider } from "./context/cartContext";
 import ReactFooter from "./components/Footer";
+import ProductDetail from "./components/SingleProduct";
 function App() {
-  
-
   return (
     <CartProvider>
-    <Router>
-      <Navbar/>
+      <Router>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Product" element={<Product />} />
+          <Route path="/product/:id" element={<ProductDetail />} />
           <Route path="/Cart" element={<Cart />} />
         </Routes>
-        <ReactFooter/>
-    </Router></CartProvider>
-  )
+        <ReactFooter />
+      </Router>
+    </CartProvider>
+  );
 }
 
-export default App
+export default App;
