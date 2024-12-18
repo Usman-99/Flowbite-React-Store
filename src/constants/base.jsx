efc0446447acca28849ef442ae3ca87c9ded28d4
Usman-99 <usmanyousuf1137@gmail.com>
2024-12-18 16:08:51 +0500

    defining page object with name and paths

--- src/constants/base.jsx
+++ src/constants/base.jsx
@@ -1,1 +1,2 @@
 export const productBaseURL="https://fakestoreapi.com"
+export const pages=[{name:"Home",path:"/"},{name:"Feedback",path:"/#Feedbackform"},{name:"Products",path:"/Product"},{name:"Cart",path:"/Cart"}]
