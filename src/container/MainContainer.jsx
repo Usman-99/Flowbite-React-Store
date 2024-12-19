b44b1be6e4f189a15b97e258cb6a7e63adb6cc7d
Usman-99 <usmanyousuf1137@gmail.com>
2024-12-19 11:01:57 +0500

    sending data from main to child routes

--- src/container/MainContainer.jsx
+++ src/container/MainContainer.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 import ReactNavbar from "../layout/Navbar";
 import ReactFooter from "../layout/Footer";
 const MainContainer = () => {
-  const text = "Personal Details";
+  const text = "Our Products";
   const items = ["hello", "world"];
   return (
     <div className="flex flex-col min-h-screen">
