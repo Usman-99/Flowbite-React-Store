6883ce04bcd4d12e495844604f9a178c2d1f9eb7
Usman-99 <usmanyousuf1137@gmail.com>
2024-12-19 11:02:47 +0500

    Rendering only allroutes component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./App.css";
-import Navbar from "./layout/Navbar";
-import Home from "./pages/Home";
-import Product from "./pages/Product";
-import Cart from "./pages/Cart";
-import { CartProvider } from "./context/cartContext";
-import ReactFooter from "./layout/Footer";
-import ProductDetail from "./pages/SingleProduct";
-function App() {
-  return (
-    <CartProvider>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/Product" element={<Product />} />
-          <Route path="/product/:id" element={<ProductDetail />} />
-          <Route path="/Cart" element={<Cart />} />
-        </Routes>
-        <ReactFooter />
-      </Router>
-    </CartProvider>
-  );
-}
+import AllRoutes from "./routes/AllRoutes"
 
-export default App;
+const App = () => {
+  return (
+    <><AllRoutes/></>
+  )
+}
+export default App
