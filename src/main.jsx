3f671aee0e8721330461adc794439064fe26a661
Usman-99 <usmanyousuf1137@gmail.com>
2024-12-19 17:36:06 +0500

    wrapping with form provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import "./App.css";
+import { FormProvider } from "./context/formContext.jsx";
 import App from "./App.jsx";
 import { CartProvider } from "./context/cartContext";
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <CartProvider>
+      <FormProvider>
       <App />
+      </FormProvider>
     </CartProvider>
   </StrictMode>
 );
