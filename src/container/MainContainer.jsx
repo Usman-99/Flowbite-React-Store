ceb8490f78212f082b2e660390126c964be4e421
Usman-99 <usmanyousuf1137@gmail.com>
2025-01-06 15:32:00 +0500

    updating

--- src/container/MainContainer.jsx
+++ src/container/MainContainer.jsx
@@ -5,7 +5,7 @@ const MainContainer = () => {
   const text = "Our Products";
   const items = ["hello", "world"];
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen bg-slate-100">
       <ReactNavbar/>
       <div className="flex-grow py-3 mt-10">
         <Outlet context={{ text, items }} />
